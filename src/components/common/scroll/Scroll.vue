6ff33ee11cac10d2a3d8b660958762d7263127c2
Ouatee_G <18483678110@163.com>
2021-03-30 23:28:55 +0800

    首页设置

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  name: "Scroll",
+  props: {
+    probeType: {
+      type: Number,
+      default: 0,
+    },
+    pullUpLoad: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      scroll: null,
+    };
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true,
+      probeType: this.probeType,
+      pullUpLoad: this.pullUpLoad,
+    });
+  },
+};
+</script>
+<style scoped>
+</style>
