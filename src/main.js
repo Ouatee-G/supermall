de2e41b0856ee473358b1e3da994bf4d49d6c7e1
Ouatee_G <18483678110@163.com>
2021-06-01 14:47:53 +0800

    插件设置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'components/common/toast'
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
@@ -11,6 +13,12 @@ Vue.prototype.$bus = new Vue()
 
 // 安装toast插件
 Vue.use(toast)
+
+// 移动端300ms延迟
+FastClick.attach(document.body)
+
+// 使用懒加载
+Vue.use(VueLazyLoad)
 
 new Vue({
   render: h => h(App),
